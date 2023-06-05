c5cd987f3a8138e05386af41b735e0aa59e34e30
Konstantin Pogodin <konstantin.pogodin@rt.ru>
2023-06-06 00:30:26 +0400

    update configs

--- configs/base.js
+++ configs/base.js
@@ -1,8 +1,8 @@
 module.exports = {
     "env": {
       "browser": true,
-      "es6": true,
       "node": true
+      "es2020": true,
     },
     "parserOptions": {
       "sourceType": "module",
--- configs/react.js
+++ configs/react.js
@@ -1,6 +1,16 @@
 module.exports = {
-    "plugins": ["react"],
+    "plugins": ["react", "jsx-a11y"],
+    "extends": ['plugin:react/recommended', "plugin:jsx-a11y/recommended"],
+    "settings": { "react": { "version": 'detect' } },
     "rules": {
-        "react/require-default-props": "off"
-    }
+        "react/require-default-props": "off",
+        "react/destructuring-assignment": "off",
+        "react/jsx-props-no-spreading": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-no-useless-fragment": "error",
+        "react/function-component-definition": [ 
+          "error",
+          { "namedComponents": "arrow-function", "unnamedComponents": "arrow-function" }
+        ],
+    },
 }
